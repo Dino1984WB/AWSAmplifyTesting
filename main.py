ea06edc85df0ed68c3314416206790a0d6d8d5e9
Dino1984WB <williamkbukowkidev@gmail.com>
2023-05-31 12:23:20 -0400

    added init py backend with a func to print http

---
+++ main.py
@@ -0,0 +1,16 @@
+#main backend file for the website
+
+import sqlite3
+import requests
+
+#print http request contents to console (debugging)
+def print_http_response(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for non-successful status codes
+
+        print("HTTP Response:")
+        print(response.text)  # Print the response content
+
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
